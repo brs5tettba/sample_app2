d0543243dd0948afbf302be08f38634ca832b4c8
Ben Arunski <gitxy.20.tall@spamgourmet.com>
2014-10-05 14:28:45 -0500

    complete chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,7 @@ class User < ActiveRecord::Base
    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
    						uniqueness: { case_sensitive: false }
 
-   before_save { self.email = email.downcase }
+   #before_save { self.email = email.downcase }
+   before_save { email.downcase! }
+
 end
